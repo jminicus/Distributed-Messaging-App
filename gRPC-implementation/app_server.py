42769a358b9781cd1263136fe2e7d0754de07250
John Minicus <john@Johns-MacBook-Pro.local>
2023-04-09 21:53:12 -0400

    added persisting DB

--- gRPC-implementation/app_server.py
+++ gRPC-implementation/app_server.py
@@ -1,5 +1,6 @@
 # SERVER SIDE OF GRPC APPLICATION
 import logging
+import sqlite3
 from concurrent import futures
 import socket
 
@@ -7,23 +8,18 @@ import grpc
 import app_pb2
 import app_pb2_grpc
 
-
 class AppServicer(app_pb2_grpc.AppServicer):
-
-    def __init__(self):
-        # will hold usernames and two entreis: first is an indicator for if the user
-        # is logged in, and second entry contains all unsent messages that should be 
-        # passed to each user when that user logs in
-        self.accounts = {}
-
 
     # creates an account if it does not already exist. Since client side will automatically
     # log in after account creation, set the log-in indicator to true
     def CreateAccount(self, request, context):
-        if request.username not in self.accounts:
-            self.accounts[request.username] = [[True],[]]
+        cur.execute("SELECT COUNT(*) FROM users WHERE username = ?",(request.username,))
+        account_exists = cur.fetchone()[0] == 0
+        if account_exists:
+            cur.execute('INSERT INTO users (username, logged_in) VALUES (?, 1)',(request.username,))
+            con.commit()
             return app_pb2.SuccessResponse(
-                success=True,
+                success=True, 
                 message="Account successfully created!"
             )
         else:
@@ -31,32 +27,44 @@ class AppServicer(app_pb2_grpc.AppServicer):
                 success=False,
                 message="Sorry, that username is taken already."
             )
-        
+    
 
     # will log in successfully if the account exists and if no other client is currently logged
     # into the account, done by checking the account dict for username key and log-in indicator
     def LogIn(self, request, context):
-        if request.username in self.accounts and not self.accounts[request.username][0]:
-            self.accounts[request.username][0] = True
-            return app_pb2.SuccessResponse(
-                success=True,
-                message="You are logged in!"
-            )
-        elif request.username in self.accounts:
-            return app_pb2.SuccessResponse(
-                success=False,
-                message="There was an issue logging in -- please make sure this account is logged out of other clients."
-            )
-        else:
+        cur.execute("SELECT COUNT(*) FROM users WHERE username = ?",(request.username,))
+        account_exists = cur.fetchone()[0] != 0
+
+        if not account_exists:
             return app_pb2.SuccessResponse(
                 success=False,
                 message="There was an issue logging in -- please make sure this account exists."
             )
-            
+
+        cur.execute("SELECT logged_in FROM users WHERE username = ?",(request.username,))
+        logged_in = cur.fetchone()[0]
+
+        if not logged_in:
+            cur.execute("UPDATE users SET logged_in = 1 WHERE username = ?",(request.username,))
+            con.commit()
+            return app_pb2.SuccessResponse(
+                success=True,
+                message="You are logged in!"
+            )
+        else:
+            return app_pb2.SuccessResponse(
+                success=False,
+                message="There was an issue logging in -- please make sure this account is logged out of other clients."
+            )
+
 
     # returns list of accounts which contain the wildcard text supplied by the client
     def ListAccounts(self, request, context):
-        for account in self.accounts:
+        cur.execute("SELECT username FROM users")
+        rows = cur.fetchall()
+        accounts = [row[0] for row in rows]
+
+        for account in accounts:
             if request.text in account:
                 yield app_pb2.Account(username=account)
 
@@ -64,8 +72,16 @@ class AppServicer(app_pb2_grpc.AppServicer):
     # "sends" message to another user by adding it to their message queue. Also verifies
     # that the recipient exists before doing so
     def SendMessage(self, request, context):
-        if request.recipient in self.accounts:
-            self.accounts[request.recipient][1].append([request.sender, request.text])
+        cur.execute("SELECT COUNT(*) FROM users WHERE username = ?",(request.recipient,))
+        recipient_exists = cur.fetchone()[0] != 0
+
+        if recipient_exists:
+            cur.execute('INSERT INTO messages (recipient, sender, text) VALUES (?, ?, ?)',(
+                request.recipient,
+                request.sender,
+                request.text,
+                ))
+            con.commit()
             return app_pb2.SuccessResponse(
                 success=True,
                 message="Message sent!"
@@ -80,19 +96,23 @@ class AppServicer(app_pb2_grpc.AppServicer):
     # empties all messages in a specified account's queue to the client side along with
     # relevant metadata
     def GetMessage(self, request, context):
-        messages = self.accounts[request.username][1]
-        self.accounts[request.username][1] = []
+        cur.execute('SELECT * FROM messages WHERE recipient = ?',(request.username,))
+        messages = cur.fetchall()
+        cur.execute('DELETE FROM messages WHERE recipient = ?',(request.username,))
+        con.commit()
+
         for message in messages:
             yield app_pb2.Message(
-                sender=message[0],
-                recipient=request.username,
-                text=message[1]
+                sender=message[1],
+                recipient=message[0],
+                text=message[2]
             )
 
 
     # logs out user by setting the account's indicator variable to false
     def LogOut(self, request, context):
-        self.accounts[request.username][0] = False
+        cur.execute("UPDATE users SET logged_in = 0 WHERE username = ?",(request.username,))
+        con.commit()
         return app_pb2.SuccessResponse(
             success=True,
             message="You have been logged out."
@@ -101,18 +121,13 @@ class AppServicer(app_pb2_grpc.AppServicer):
 
     # deletes account by simply deleting account's key in dictionary
     def DeleteAccount(self, request, context):
-        try:
-            del self.accounts[request.username]
-            return app_pb2.SuccessResponse(
-                success=True,
-                message="Account deleted. Goodbye!"
-            )
-        except KeyError:
-            return app_pb2.SuccessResponse(
-                success=False,
-                message="Unkown error occured."
-            )
-
+        cur.execute("DELETE FROM users WHERE username = ?",(request.username,))
+        con.commit()
+        return app_pb2.SuccessResponse(
+            success=True,
+            message="Account deleted. Goodbye!"
+        )
+    
 
 # run server with 10 threads to handle multiple clients
 def serve():
@@ -129,5 +144,13 @@ def serve():
 
 
 if __name__ == '__main__':
+    # setup conneciton to database
+    con = sqlite3.connect("store.db", check_same_thread=False)
+    cur = con.cursor()
+
     logging.basicConfig()
     serve()
+
+    # close cursor and database connection
+    cur.close()
+    con.close()
